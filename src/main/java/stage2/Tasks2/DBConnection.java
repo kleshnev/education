b6267aafdf94771bf34979149f1ec98c2f231052
kleshnev <kleshnevden@mail.ru>
2022-04-19 18:42:57 +0400

    Added sql inquiries for Db task

--- src/main/java/stage2/Tasks2/DBConnection.java
+++ src/main/java/stage2/Tasks2/DBConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    private static final String path = System.getProperty("user.dir");
     private static final String pathToDB =
-            "jdbc:h2:file:C:/Users/Denis/IdeaProjects/education/database;AUTO_SERVER=TRUE";
+            "jdbc:h2:file:"+path+"/database;AUTO_SERVER=TRUE";
     public Connection getDBConnection() throws SQLException {
         return DriverManager.getConnection(pathToDB, "sa", "");
     }
-
 }
