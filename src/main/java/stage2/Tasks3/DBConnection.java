60f19315effc758189d975163378e9a029830018
kleshnev <kleshnevden@mail.ru>
2022-04-23 23:19:13 +0400

    DB paths refactor

--- src/main/java/stage2/Tasks3/DBConnection.java
+++ src/main/java/stage2/Tasks3/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String path = System.getProperty("user.dir");
-    private static final String pathToDB = "jdbc:h2:file:"+path+"/Task3/database;AUTO_SERVER=TRUE";
+    private static final String pathToDB = "jdbc:h2:file:"+path+"/Task3DB/database;AUTO_SERVER=TRUE";
     public Connection getDBConnection() throws SQLException {
         return DriverManager.getConnection(pathToDB, "sa", "");
     }
